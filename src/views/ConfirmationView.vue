1612885e337a70a2785d7d9c9544f815a10c437b
damnsope <darylchua@hotmail.sg>
2022-11-13 00:46:29 +0800

    for rapie

--- src/views/ConfirmationView.vue
+++ src/views/ConfirmationView.vue
@@ -1,76 +1,55 @@
 <template>
-    <div class="q-pa-l row justify-center items-start q-gutter-md">
-        <div class="col text-center">     
-        <q-card class="card" >
-        <q-card-section class="bg-primary text-white">
-            <!-- add a confirmation logo  -->
-            <div>
-                <div class="logo">
-                    <q-icon name="check" size="10vh"/>
+    <div class="container q-pa-xl">
+        <div class="bg"></div>
+        <q-card class="card q-mx-auto" bordered dark>
+            <q-card-section class="text-center">
+                <q-icon name="check" size="10vh" />
+                <h3 class="q-py-md">Booking Confirmed</h3>
+                <div class="info font">We are pleased to inform you that your booking has been confirmed! See you
+                    then! 😊
                 </div>
-                <h3 style="padding-top:10px; padding-bottom: 30px;">Booking Confirmed</h3>
-                <div class="info">
-                    <p>"We are please to inform you that booking request have been received and confirm! We can't wait to see you there!"</p>
-                </div>
-                <!-- <q-separator inset dark />   -->
-            </div>
             </q-card-section>
-            
+
             <q-card-section>
-
-            <div class="text-left q-pa-sm">
-                <h4 style="color:black; padding-bottom:0; padding-top:20px;">Booking Details</h4>
-            </div>
-
-            <!-- Package name, date and timing data retrive from firebase db -->
-
-            <div class="font row w-40 items-center one">
-                <div class="col-6 text-right q-pa-sm text-bold">Package</div>
-                <div class="col-6 text-left q-pa-sm">Package name</div>
-            </div>
-            <div class="font row w-40 items-center one">
-                <div class="col-6 text-right q-pa-sm text-bold">Date</div>
-                <div class="text-subtitle2 col-6 text-left q-pa-sm">Friday, November 11th</div>
-            </div>
-            <div class="font row w-40 items-center one">
-                <div class="col-6 text-right q-pa-sm text-bold">Timing</div>
-                <div class="text-subtitle2 col-6 text-left q-pa-sm">6pm-8pm</div>
-            </div>
-
+                <div class="color font">
+                    <!-- Package name, date and timing data retrive from firebase db -->
+                    <div class="row items-center" style="height: 100%;">
+                        <div class="col col-lg-4 text-center">
+                            Package
+                            <div class="formatText">
+                                {\{ Package name here }\}
+                            </div>
+                        </div>
+                        <div class="col col-lg-4 text-center">
+                            Date
+                            <div class="formatText">
+                                Friday, November 11th
+                            </div>
+                        </div>
+                        <div class="col col--4 text-center">
+                            Timing
+                            <div class="formatText">
+                                6pm-8pm
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </q-card-section>
-            <!-- <q-separator dark /> -->
-
-            <div class='row w-40 justify-center one'>
-            <q-card-actions style="display:flex">
+                <q-card-actions class="justify-center q-pa-md">
                     <!-- when making another booking, go back to the original page  -->
-                    <router-link :to="{ path: '/simulator' }" style="display: inline-block;text-decoration:none;">
-                        <q-btn
-                            label="Make another booking"
-                            type="submit"
-                            color="primary"
-                            rounded
-                            no-caps
-                            class="q-my-md q-ma-md"
-                        ></q-btn>
-                    </router-link>
-                
+                    <q-btn label="Make another booking" type="submit" color="primary" rounded no-caps class="q-mx-md">
+                        <router-link :to="{ path: '/simulator' }" />
+                    </q-btn>
+
                     <!-- when it close go to the homepage -->
-                    <router-link :to="{ path: '/'}" style="display: inline-block;text-decoration:none;">
-                        <q-btn
-                            label="Confirm"
-                            type="Back"
-                            color="primary"
-                            rounded
-                            no-caps
-                            class="q-my-md"
-                        ></q-btn>
-                    </router-link>
-                   
-            </q-card-actions>
-            </div> 
-        
+                    <q-btn label="Confirm (?? confirm alr no meh)" type="Back" color="primary" rounded no-caps
+                        class="q-mx-md">
+                        <router-link :to="{ path: '/' }" />
+                    </q-btn>
+
+                </q-card-actions>
+
         </q-card>
-    </div>
     </div>
 </template>
 
@@ -85,40 +64,51 @@ const store = useStore();
 console.log(store)
 
 export default {
-    name: 'confirm-card', 
+    name: 'confirm-card',
     // props: 
 }
 </script>
 
-<style lang="scss" scoped>
-    .date{
-        text-align: center;
-        /* font-weight: 100; */
-        padding-top: 20px;
-    }
+<style lang="scss">
+.info {
+    padding: 0 20%;
+    text-align: center;
+}
 
-    .click{
-        align-items: center;
-        display: flex;
-    }
+.color {
+    border-radius: 15px;
+    background-color: rgb(50, 50, 50);
+    height: 100px;
+}
 
-    .info{
-        padding-left: 20%;
-        padding-right: 20%;
-    }
+.formatText {
+    text-align: center;
+    font-size: 16px;
+    font-weight: bold;
+}
 
-    .font {
+.font {
     font-family: 'Open Sans', sans-serif;
-    margin:0;}
+}
 
-    .one{
-        display: flex;
-    }
+.card {
+    border-radius: 30px;
+    width: 70%;
+}
 
-    .card{
-        border-radius: 30px;
-        width: 65%;
-    }
-
+.bg {
+    position: absolute;
+    background: url('@/assets/summary-bg.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 1;
+    z-index: -1;
+    filter: brightness(30%);
+}
 </style>
 
